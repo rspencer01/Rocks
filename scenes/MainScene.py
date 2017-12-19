247f2041a565577f2213b28a47cd32b1ef470d2f
Robert Spencer <robert.spencer94@gmail.com>
2017-12-20 00:00:41 +0200

    Upgrade to fit with dent 0.0.5

--- scenes/MainScene.py
+++ scenes/MainScene.py
@@ -1,28 +1,20 @@
 import RockPlacement
-from dent.Scene import Scene
-import dent.Texture as Texture
-from dent.Object import Object
-from dent.RenderStage import RenderStage
-from dent.RectangleObjects import RectangleObject, BlankImageObject
-from dent.ActionController import ActionController
+from dent.Scene import DeferredRenderScene
+from dent.RectangleObjects import RectangleObject
 import dent.Shaders
 from dent.Shadows import Shadows
 import numpy as np
 import dent.transforms
 import dent.messaging
-import dent.args
 import random
 
 import OpenGL.GL as gl
 import Rock
 
-class MainScene(Scene):
+class MainScene(DeferredRenderScene):
   def __init__(self):
     super(MainScene, self).__init__()
     gl.glEnable(gl.GL_CULL_FACE)
-    self.renderPipeline.stages += [
-        RenderStage(render_func=self.display, aux_buffer=True),
-        RenderStage(render_func=self.lighting_display   , clear_depth=False  , final_stage=True)]
 
     RockPlacement.initialise([3*3,9*9,21*21,51*51])
     self.rock_lod_0 = Rock.Rock(RockPlacement.output_buffers[0], 9,35)
@@ -36,7 +28,7 @@ class MainScene(Scene):
     self.cameraSpeed = 50.
 
     self.floor = RectangleObject('floor')
-    self.sky = BlankImageObject(0.4, 0.5, 0.6)
+    self.backgroundColor = np.array([0.4, 0.5, 0.6])
 
     dent.messaging.add_handler('timer', self.timer)
     dent.messaging.add_handler('keyboard', self.key)
@@ -46,11 +38,6 @@ class MainScene(Scene):
 
     self.shadows = Shadows(self.render_all, self.camera, rng=5)
     self.shadows.shadowCamera.rotUpDown(0.5)
-
-    self.lighting_rectangle = RectangleObject('lighting')
-    self.lighting_rectangle.shader['colormap'] = Texture.COLORMAP_NUM
-    self.lighting_rectangle.shader['normalmap'] = Texture.COLORMAP2_NUM
-    self.lighting_rectangle.shader['positionmap'] = Texture.COLORMAP3_NUM
 
   def timer(self, fps):
     self.time += 1./fps
@@ -85,7 +72,6 @@ class MainScene(Scene):
     dent.Shaders.setUniform('projection', projection)
 
     self.camera.render()
-    self.sky.display()
     self.render_all()
 
   def render_all(self):
@@ -94,9 +80,3 @@ class MainScene(Scene):
     self.rock_lod_1.display()
     self.rock_lod_2.display()
     self.rock_lod_3.display()
-
-  def lighting_display(self, previous_stage, **kwargs):
-    previous_stage.displayColorTexture.load()
-    previous_stage.displaySecondaryColorTexture.load()
-    previous_stage.displayAuxColorTexture.load()
-    self.lighting_rectangle.display()
